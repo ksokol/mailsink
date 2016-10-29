5034a1ba000adeb055bb278a1f53c3e3bb6ef8f0
Kamill Sokol <dev@sokol-web.de>
2016-10-29 11:33:01 +0200

    Add initial Spring Boot 1.4.1 application

---
+++ src/main/java/com/github/ksokol/mailsink/MailsinkApplication.java
@@ -0,0 +1,12 @@
+package com.github.ksokol.mailsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailsinkApplication.class, args);
+    }
+}
