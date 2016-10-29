e56b9aed56461010378454408c83fd67ae7cc58d
Kamill Sokol <dev@sokol-web.de>
2016-10-29 12:37:25 +0200

    Add implementation to MessageListener::deliver

--- src/main/java/com/github/ksokol/mailsink/handler/MessageListener.java
+++ src/main/java/com/github/ksokol/mailsink/handler/MessageListener.java
@@ -1,5 +1,7 @@
 package com.github.ksokol.mailsink.handler;
 
+import com.github.ksokol.mailsink.entity.Mail;
+import com.github.ksokol.mailsink.repository.MailRepository;
 import org.springframework.stereotype.Component;
 import org.subethamail.smtp.helper.SimpleMessageListener;
 
@@ -12,6 +14,12 @@ import java.io.InputStream;
 @Component
 public class MessageListener implements SimpleMessageListener {
 
+    private final MailRepository mailRepository;
+
+    public MessageListener(MailRepository mailRepository) {
+        this.mailRepository = mailRepository;
+    }
+
     @Override
     public boolean accept(String from, String recipient) {
         return true;
@@ -19,6 +27,9 @@ public class MessageListener implements SimpleMessageListener {
 
     @Override
     public void deliver(String from, String recipient, InputStream body) throws IOException {
-
+        Mail mail = new Mail();
+        mail.setSender(from);
+        mail.setRecipient(recipient);
+        mailRepository.save(mail);
     }
 }
--- src/test/java/com/github/ksokol/mailsink/handler/MessageListenerTest.java
+++ src/test/java/com/github/ksokol/mailsink/handler/MessageListenerTest.java
@@ -1,13 +1,19 @@
 package com.github.ksokol.mailsink.handler;
 
+import com.github.ksokol.mailsink.entity.Mail;
+import com.github.ksokol.mailsink.repository.MailRepository;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Kamill Sokol
@@ -18,14 +24,21 @@ public class MessageListenerTest {
     @InjectMocks
     private MessageListener messageListener;
 
+    @Mock
+    private MailRepository mailRepository;
+
     @Test
     public void shouldAcceptEveryMessage() throws Exception {
         assertThat(messageListener.accept("irrelevant", "irrelevant"), is(true));
     }
 
     @Test
-    public void deliver() throws Exception {
+    public void shouldPersistRecieviedMessageInMailRepository() throws Exception {
+        messageListener.deliver("from", "recipient", null);
 
+        verify(mailRepository).save(argThat(Matchers.<Mail>allOf(
+                hasProperty("sender", is("from")),
+                hasProperty("recipient", is("recipient"))
+        )));
     }
-
 }
