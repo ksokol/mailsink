95c4f7f618bfb2722570fc57668fd3a5ba6b2c65
Kamill Sokol <dev@sokol-web.de>
2016-10-30 12:11:09 +0100

    Convert subethamail message InputStream in MessageListener before persisting to MailRepository

--- src/main/java/com/github/ksokol/mailsink/handler/MessageListener.java
+++ src/main/java/com/github/ksokol/mailsink/handler/MessageListener.java
@@ -1,6 +1,5 @@
 package com.github.ksokol.mailsink.handler;
 
-import com.github.ksokol.mailsink.entity.Mail;
 import com.github.ksokol.mailsink.repository.MailRepository;
 import org.springframework.stereotype.Component;
 import org.subethamail.smtp.helper.SimpleMessageListener;
@@ -15,9 +14,11 @@ import java.io.InputStream;
 public class MessageListener implements SimpleMessageListener {
 
     private final MailRepository mailRepository;
+    private final MailConverter mailConverter;
 
-    public MessageListener(MailRepository mailRepository) {
+    public MessageListener(MailRepository mailRepository, MailConverter mailConverter) {
         this.mailRepository = mailRepository;
+        this.mailConverter = mailConverter;
     }
 
     @Override
@@ -27,9 +28,6 @@ public class MessageListener implements SimpleMessageListener {
 
     @Override
     public void deliver(String from, String recipient, InputStream body) throws IOException {
-        Mail mail = new Mail();
-        mail.setSender(from);
-        mail.setRecipient(recipient);
-        mailRepository.save(mail);
+        mailRepository.save(mailConverter.convert(body));
     }
 }
--- src/test/java/com/github/ksokol/mailsink/handler/MessageListenerTest.java
+++ src/test/java/com/github/ksokol/mailsink/handler/MessageListenerTest.java
@@ -2,17 +2,23 @@ package com.github.ksokol.mailsink.handler;
 
 import com.github.ksokol.mailsink.entity.Mail;
 import com.github.ksokol.mailsink.repository.MailRepository;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.hamcrest.Matchers.hasProperty;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -27,18 +33,41 @@ public class MessageListenerTest {
     @Mock
     private MailRepository mailRepository;
 
+    @Mock
+    private MailConverter mailConverter;
+
     @Test
     public void shouldAcceptEveryMessage() throws Exception {
         assertThat(messageListener.accept("irrelevant", "irrelevant"), is(true));
     }
 
     @Test
-    public void shouldPersistRecieviedMessageInMailRepository() throws Exception {
-        messageListener.deliver("from", "recipient", null);
+    public void shouldCallMailConverter() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream(new byte[]{});
 
-        verify(mailRepository).save(argThat(Matchers.<Mail>allOf(
-                hasProperty("sender", is("from")),
-                hasProperty("recipient", is("recipient"))
-        )));
+        messageListener.deliver("irrelevant", "irrelevant", inputStream);
+
+        verify(mailConverter).convert(inputStream);
+    }
+
+    @Test
+    public void shouldPersistReceivedMessageInMailRepository() throws Exception {
+        Mail mail = new Mail();
+
+        given(mailConverter.convert(any())).willReturn(mail);
+
+        messageListener.deliver("irrelevant", "irrelevant", null);
+
+        verify(mailRepository).save(argThat(is(mail)));
+    }
+
+    @Test
+    public void shouldConvertReceivedMessaageBeforeSavingInMailRepository() throws Exception {
+        messageListener.deliver("irrelevant", "irrelevant", null);
+
+        InOrder callOrder = inOrder(mailConverter, mailRepository);
+
+        callOrder.verify(mailConverter).convert(null);
+        callOrder.verify(mailRepository).save(Mockito.<Mail>any());
     }
 }
