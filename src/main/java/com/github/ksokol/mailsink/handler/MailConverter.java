6c157d96df03402df3726138d28bece26309aa3a
Kamill Sokol <dev@sokol-web.de>
2016-10-30 12:10:20 +0100

    Add MailConverter: convert subethamail message InputStream to Mail instance

---
+++ src/main/java/com/github/ksokol/mailsink/handler/MailConverter.java
@@ -0,0 +1,74 @@
+package com.github.ksokol.mailsink.handler;
+
+import com.github.ksokol.mailsink.entity.Mail;
+import org.apache.commons.io.IOUtils;
+import org.apache.james.mime4j.dom.Message;
+import org.apache.james.mime4j.dom.TextBody;
+import org.apache.james.mime4j.dom.address.Address;
+import org.apache.james.mime4j.dom.address.Mailbox;
+import org.apache.james.mime4j.message.MessageBuilder;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Kamill Sokol
+ */
+@Component
+public class MailConverter implements Converter<InputStream, Mail> {
+
+    @Override
+    public Mail convert(InputStream source) {
+        try {
+            return convertInternal(source);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception.getMessage(), exception);
+        }
+    }
+
+    private Mail convertInternal(InputStream source) throws IOException {
+        Message message = new MessageBuilder().parse(source).build();
+        Mail target = new Mail();
+
+        setSender(message, target);
+        setRecipient(message, target);
+        setSubject(message, target);
+        setCreatedAt(message, target);
+        setBody(message, target);
+
+        return target;
+    }
+
+    private void setSender(Message source, Mail target) {
+        Mailbox mailbox = source.getFrom().get(0);
+        if(mailbox.getName() != null) {
+            target.setSender(String.format("%s <%s>", mailbox.getName(), mailbox.getAddress()));
+        } else {
+            target.setSender(mailbox.getAddress());
+        }
+    }
+
+    private void setRecipient(Message source, Mail target) {
+        //TODO add support for multiple recipients
+        Address address = source.getTo().get(0);
+        target.setRecipient(address.toString());
+    }
+
+    private void setSubject(Message source, Mail target) {
+        target.setSubject(source.getSubject());
+    }
+
+    private void setBody(Message source, Mail target) throws IOException {
+        //TODO add support for HTML
+        if("text/plain".equals(source.getMimeType()) && source.getBody() instanceof TextBody) {
+            TextBody textBody = (TextBody) source.getBody();
+            target.setBody(IOUtils.toString(textBody.getReader()).trim());
+        }
+    }
+
+    private void setCreatedAt(Message source, Mail target) {
+        target.setCreatedAt(source.getDate());
+    }
+}
---
+++ src/test/java/com/github/ksokol/mailsink/handler/MailConverterTest.java
@@ -0,0 +1,100 @@
+package com.github.ksokol.mailsink.handler;
+
+import com.github.ksokol.mailsink.entity.Mail;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Kamill Sokol
+ */
+public class MailConverterTest {
+
+    private MailConverter converter = new MailConverter();
+
+    private Mail mail;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldConvertIOExceptionToRuntimeException() throws Exception {
+        InputStream body = mock(InputStream.class, new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                throw new IOException("expected exception");
+            }
+        });
+
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("expected exception");
+
+        converter.convert(body);
+    }
+
+    @Test
+    public void shouldExtractSenderAndDisplayNameFromPlainTextMail() throws Exception {
+        givenMail("plain1.txt");
+        assertThat(mail.getSender(), is("Display Name <sender@localhost>"));
+    }
+
+    @Test
+    public void shouldExtractSenderFromPlainTextMail() throws Exception {
+        givenMail("plain2.txt");
+        assertThat(mail.getSender(), is("sender@localhost"));
+    }
+
+    @Test
+    public void shouldExtractOneRecipientFromPlainTextMail() throws Exception {
+        givenMail("plain1.txt");
+        assertThat(mail.getRecipient(), is("recipient@localhost"));
+    }
+
+    @Test
+    public void shouldIgnoreSecondRecipientInPlainTextMail() throws Exception {
+        givenMail("plain2.txt");
+        assertThat(mail.getRecipient(), is("recipient1@localhost"));
+    }
+
+    @Test
+    public void shouldExtractSubjectFromPlainTextMail() throws Exception {
+        givenMail("plain1.txt");
+        assertThat(mail.getSubject(), is("Subject"));
+    }
+
+    @Test
+    public void shouldNotExtractHtmlBodyFromHtmlMail() throws Exception {
+        givenMail("plain2.txt");
+        assertThat(mail.getBody(), nullValue());
+    }
+
+    @Test
+    public void shouldExtractBodyFromPlainTextMail() throws Exception {
+        givenMail("plain1.txt");
+        assertThat(mail.getBody(), is("Mail body\r\n new line\r\n another line"));
+    }
+
+    @Test
+    public void shouldExtractDateFromPlainTextMail() throws Exception {
+        givenMail("plain1.txt");
+        assertThat(mail.getCreatedAt(), is(Date.from(LocalDateTime.of(2016,10,30,10,10,10).toInstant(ZoneOffset.UTC))));
+    }
+
+    private void givenMail(String fileName) throws IOException {
+        mail = converter.convert(new ClassPathResource(fileName).getInputStream());
+    }
+}
