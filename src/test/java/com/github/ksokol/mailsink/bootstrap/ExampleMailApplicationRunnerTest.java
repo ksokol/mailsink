41710f50ebc4ea83d2705ca62adb058cef9f8b1e
Kamill Sokol <dev@sokol-web.de>
2020-07-17 22:48:14 +0200

    chore: Fix tests in IntelliJ.

--- src/test/java/com/github/ksokol/mailsink/bootstrap/ExampleMailApplicationRunnerTest.java
+++ src/test/java/com/github/ksokol/mailsink/bootstrap/ExampleMailApplicationRunnerTest.java
@@ -5,25 +5,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 
 /**
  * @author Kamill Sokol
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = BEFORE_CLASS)
 public class ExampleMailApplicationRunnerTest {
 
     @Autowired
     private MailRepository mailRepository;
 
     @Test
-    public void shouldImportExampleMailsAfterStartup() throws Exception {
+    public void shouldImportExampleMailsAfterStartup() {
         assertThat(mailRepository.findAll(),
                 hasItems(
                     hasProperty("subject", is("mail1")),
--- src/test/java/com/github/ksokol/mailsink/configuration/CacheHeaderFilterTest.java
+++ src/test/java/com/github/ksokol/mailsink/configuration/CacheHeaderFilterTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.mail.port=13025"})
 public class CacheHeaderFilterTest {
 
     @Autowired
